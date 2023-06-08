d149a0142e074409f1a5a9740ef8f25f9a452a45
AgungPutra220903 <116995331+AgungPutra220903@users.noreply.github.com>
2023-06-08 17:17:41 +0800

    Add files via upload

---
+++ Mahasiswa.sql
@@ -0,0 +1,38 @@
+/*
+SQLyog Ultimate - MySQL GUI v8.21 
+MySQL - 5.5.5-10.4.27-MariaDB : Database - terserah
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`terserah` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+
+USE `terserah`;
+
+/*Table structure for table `mahasiswa` */
+
+DROP TABLE IF EXISTS `mahasiswa`;
+
+CREATE TABLE `mahasiswa` (
+  `Nim` varchar(12) NOT NULL,
+  `Nama` varchar(100) NOT NULL,
+  `Jk` varchar(10) NOT NULL,
+  `Alamat` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Data for the table `mahasiswa` */
+
+insert  into `mahasiswa`(`Nim`,`Nama`,`Jk`,`Alamat`) values ('2201010763','I Gusti Agung Eka Putra Wanda','Pria','Singaraja');
+insert  into `mahasiswa`(`Nim`,`Nama`,`Jk`,`Alamat`) values ('2201010783','Surya Imanuel','Pria','Denpasar');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
