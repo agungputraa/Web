4b8d10debd7d2e2a5fb1a829febc34061e7f1348
AgungPutra220903 <116995331+AgungPutra220903@users.noreply.github.com>
2023-06-09 15:27:28 +0700

    Create initfb.js

---
+++ initfb.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDfQal5qQ3zS3DA21oeTrRAonislCWuX68",
+    authDomain: "blogs-233c7.firebaseapp.com",
+    databaseURL: "https://blogs-233c7.firebaseio.com",
+    projectId: "blogs-233c7",
+    storageBucket: "blogs-233c7.appspot.com",
+    messagingSenderId: "960693622725",
+    appId: "1:960693622725:web:325e29579c6c3dc4d91dac",
+    measurementId: "G-3MPBFX087D"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
