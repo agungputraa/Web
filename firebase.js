dc843630ebc53070bdd63870a857ce9f5288e1bf
AgungPutra220903 <116995331+AgungPutra220903@users.noreply.github.com>
2023-06-09 15:10:12 +0700

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "https://bot-wa-237e9-default-rtdb.firebaseio.com",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
