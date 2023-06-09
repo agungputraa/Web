4bfd188a3f5609b2fec7dcfcc7947bea74b8e82d
AgungPutra220903 <116995331+AgungPutra220903@users.noreply.github.com>
2023-06-09 15:23:42 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,12 @@
-// Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "https://bot-wa-237e9-default-rtdb.firebaseio.com",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: ""
+var firebaseConfig = {
+    apiKey: "AIzaSyDfQal5qQ3zS3DA21oeTrRAonislCWuX68",
+    authDomain: "blogs-233c7.firebaseapp.com",
+    databaseURL: "https://blogs-233c7.firebaseio.com",
+    projectId: "blogs-233c7",
+    storageBucket: "blogs-233c7.appspot.com",
+    messagingSenderId: "960693622725",
+    appId: "1:960693622725:web:325e29579c6c3dc4d91dac",
+    measurementId: "G-3MPBFX087D"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
