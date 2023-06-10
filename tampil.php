d7ec4bf2fce6742e402fdb5c1b956b523f12ce7e
AgungPutra220903 <116995331+AgungPutra220903@users.noreply.github.com>
2023-06-10 17:40:13 +0800

    Update tampil.php

--- tampil.php
+++ tampil.php
@@ -17,22 +17,26 @@
 $sql = "SELECT Nim, Nama, Jk, Alamat FROM mahasiswa";
 $result = mysqli_query($conn, $sql);
 
+
 echo "<table>";
 if (mysqli_num_rows($result) > 0) {
   // output data of each row
-  
-  while($row = mysqli_fetch_assoc($result)) {
-    echo "<tr>";
-    echo "<td>Nim: " . $row["Nim"]."</td><br>";
-    echo "<td>Nama: " . $row["Nama"]."</td><br>";
-    echo "<td>Jenis Kelamin: " . $row["Jk"]."</td><br>";
-    echo "<td>Alamat :" . $row["Alamat"]."</td><br><br>";
-    echo "</tr>";
+  while ($row = mysqli_fetch_assoc($result)){
+      echo "<tr>";
+      echo "<td>" . $row["Nim"]."</td>";
+      echo "<td>" . $row["Nama"]."</td>";
+      echo "<td>" . $row["Jk"]."</td>";
+      echo "<td>" . $row["Alamat"]."</td>";
+      echo "<td><a href=\"hapus.php?Nim=".$row["Nim"]."\">Hapus</a></td>";
+      echo "<td><a href=\"ubah.php?Nim=".$row["Nim"]."\">Ubah</a></td>";
+      echo "</tr> ";
   }
+  
 } else {
   echo "0 results";
 }
 echo "</table>";
+
 mysqli_close($conn);
 ?>
 </body>
